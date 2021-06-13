b65bbe8a98a3e59a5b2bc1462e92e0b383698dff
Srihari Ayapilla <Srihari Ayapilla>
2021-06-13 15:50:38 +0530

    null safety

--- lib/src/animated_radio_button_item.dart
+++ lib/src/animated_radio_button_item.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 class AnimatedRadioButtonItem {
   final String label;
-  final Color color;
+  final Color? color;
   final fillInColor;
-  final TextStyle labelTextStyle;
+  final TextStyle? labelTextStyle;
 
   AnimatedRadioButtonItem({
-    @required this.label,
+    required this.label,
     this.color,
     this.fillInColor,
     this.labelTextStyle,
@@ -17,11 +17,11 @@ class AnimatedRadioButtonItem {
   String get getLabel => label;
 
   /// color property used to set the color of individual radio button
-  Color get getColor => color;
+  Color? get getColor => color;
 
   /// filInColor property used to set the color inside the ring of a radio button
-  Color get getFillInColor => fillInColor;
+  Color? get getFillInColor => fillInColor;
 
   /// labelTextStyle property defines the style of the label text
-  TextStyle get getLabelTextStyle => labelTextStyle;
+  TextStyle? get getLabelTextStyle => labelTextStyle;
 }
